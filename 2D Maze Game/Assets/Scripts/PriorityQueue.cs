2247bc5aedc6380c3cca5030df5346f9c4ab928d
DariaMilczarska <daria.milczarska@gmail.com>
2021-09-11 16:30:41 +0200

    Priority Queue bug fix - not adding smallest element to the queue bug fixed

--- 2D Maze Game/Assets/Scripts/PriorityQueue.cs
+++ 2D Maze Game/Assets/Scripts/PriorityQueue.cs
@@ -2,23 +2,29 @@
 
 public class PriorityQueue<T1, T2>
 {
-    private List<KeyValuePair<T1, T2>> queue = new List<KeyValuePair<T1, T2>>();
+    public List<KeyValuePair<T1, T2>> queue = new List<KeyValuePair<T1, T2>>();
     private int size;
-    public void Enqueue(T2 value, T1 key)
+    public void Enqueue(T1 key, T2 value)
     {
         size = queue.Count;
-        if(size == 0 || Compare<T2>(value, queue[size - 1].Value))
+        if (size == 0 || Compare<T2>(value, queue[size - 1].Value))
         {
             queue.Add(new KeyValuePair<T1, T2>(key, value));
         }
+        else if (Compare<T2>(queue[0].Value, value))
+        {
+            queue.Insert(0, new KeyValuePair<T1, T2>(key, value));
+        }
         else
         {
-            for (int index = 0; index < queue.Count; ++index)
+            for (int index = 0; index < queue.Count - 1; ++index)
             {
                 KeyValuePair<T1, T2> element = queue[index];
-                if (Compare<T2>(value, element.Value))
+                KeyValuePair<T1, T2> nextElement = queue[index + 1];
+                if (Compare<T2>(value, element.Value) && Compare<T2>(nextElement.Value, value))
                 {
-                    queue.Insert(index, (new KeyValuePair<T1, T2>(key, value)));
+                    queue.Insert(index + 1, (new KeyValuePair<T1, T2>(key, value)));
+                    break;
                 }
             }
         }
@@ -38,6 +44,6 @@ public class PriorityQueue<T1, T2>
     public static bool Compare<T>(T v1, T v2)
     {
         IComparer<T> comparer = Comparer<T>.Default;
-        return comparer.Compare(v1, v2) < 0 ? true : false;
+        return comparer.Compare(v1, v2) < 0 ? false : true;
     }
 }
